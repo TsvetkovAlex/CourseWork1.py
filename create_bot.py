8ea198ca4f6ed2e944515c41b954c7558fd55a5a
Alex Tsvetkov <sachatsvetkov@gmail.com>
2023-06-16 19:06:12 +0300

    commite2

--- create_bot.py
+++ create_bot.py
@@ -6,7 +6,7 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 storage = MemoryStorage()
 
-bot = Bot(token='6132100459:AAE3zGmjHjwwNuT3aLRgyUKRqgsvJbn6QKs')
+bot = Bot(token='6052705571:AAGCexmk6Y7ix-pzMJdL-tev2QGb2O7uUSo')
 ADMINS_CHAT_ID = -1001962943242
 
 dp = Dispatcher(bot, storage=storage)
